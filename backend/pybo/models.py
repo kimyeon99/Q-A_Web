e2aa6a716c96a7e1f44afb54b5dc3938d4185a8f
kimyeon99 <tmdwo7017@naver.com>
2022-09-19 22:36:23 +0900

    Add - delete, modify

--- backend/pybo/models.py
+++ backend/pybo/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField()
-
+    modify_date = models.DateTimeField(null=True, blank=True)
     def __str__(self):
         return self.subject # 조회 시 대표값
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
--- backend/pybo/views.py
+++ backend/pybo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.http import HttpResponseNotAllowed
-from .models import Question
+from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 #render에서 question_list -> question_list.html 으로 템플릿 변환
 def index(request):
@@ -13,35 +15,40 @@ def index(request):
     paginator = Paginator(question_list, 10) # 페이지당 10개씩
     # 장고 내부적으로는 데이터 전체를 조회하지 않고 해당 페이지의 데이터만 조회하도록 쿼리가 변경
     page_obj = paginator.get_page(page)
-
     context = {'question_list':page_obj}
     return render(request, 'pybo/question_list.html', context)
+
 def detail(request, question_id):
     # Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id) #pk=primary key
     context = {'question':question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
-            answer.create_date = timezone.now()
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.question = question
+            answer.create_date = timezone.now()
             answer.save()
             return redirect('pybo:detail', question_id=question_id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        # return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question':question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():  # 폼이 유효하다면
             question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.author = request.user
             question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
             question.save()  # 데이터를 실제로 저장한다.
             return redirect('pybo:index')
@@ -50,3 +57,58 @@ def question_create(request):
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
     # return render(request, 'pybo/question_form.html', {'form': form})
+
+@login_required(login_url="common:login")
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정 권한x') #  messages는 장고가 제공하는 모듈로 넌필드 오류(non-field error)를 발생시킬 경우에 사용한다.
+        return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id = question.id)
+    else:
+            #instance 값을 지정하면 폼의 속성 값이 instance의 값으로 채워진다.
+            form = QuestionForm(instance=question)
+            context = {'form':form}
+            return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '노 권한')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+        context = {'answer':answer, 'form':form}
+        return render(request, 'pybo/answer_form.html', context)
+
+@login_required(login_url="common:login")
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '노 권한2')
+    else:
+        answer.delete()
+    redirect('pybo:detail', question_id=answer.question.id)
